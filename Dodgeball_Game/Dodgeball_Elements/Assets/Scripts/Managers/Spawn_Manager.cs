05dd813f049e0ace6d1dd75384e24ed877e5128b
JonM284 <44879704+JonM284@users.noreply.github.com>
2020-10-02 19:12:34 +0900

    Cleaning
    Removed unnecessary files.
    Added behaviours to player and such

---
+++ Dodgeball_Game/Dodgeball_Elements/Assets/Scripts/Managers/Spawn_Manager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Manager : MonoBehaviour
+{
+
+    public List<Transform> Level_Spawn_Positions;
+    public List<Transform> Usable_Spawn_Positions;
+    public List<Player_Movement> all_Players;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Set_Initial_Spawn_Points();
+        Set_Usable_Spawn_Points();
+        Find_All_Players();
+        Set_Player_Spawn_Position();
+    }
+
+
+    void Set_Initial_Spawn_Points()
+    {
+        foreach (Transform child in transform)
+        {
+            Level_Spawn_Positions.Add(child);
+        }
+    }
+
+    void Set_Usable_Spawn_Points()
+    {
+        foreach (Transform t in Level_Spawn_Positions)
+        {
+            Usable_Spawn_Positions.Add(t);
+        }
+    }
+
+    void Find_All_Players()
+    {
+        foreach (Player_Movement player in Resources.FindObjectsOfTypeAll(typeof(Player_Movement)))
+        {
+            all_Players.Add(player);
+           
+        }
+    }
+
+    /// <summary>
+    /// Give calling player a random spawn position.
+    /// </summary>
+    /// <param name="_player"></param>
+    public void Set_Player_Spawn_Position()
+    {
+        for (int i = 0; i < all_Players.Count; i++)
+        {
+            int random_Spawn = Random.Range(0, Usable_Spawn_Positions.Count);
+            all_Players[i].transform.position = Usable_Spawn_Positions[random_Spawn].position;
+            Usable_Spawn_Positions.RemoveAt(random_Spawn);
+        }
+    }
+
+
+    IEnumerator Spawn_Sequence()
+    {
+        for (int i = 0; i < all_Players.Count; i++)
+        {
+            //set each player to do spawn animation
+            yield return new WaitForSeconds(0.3f);
+        }
+
+    }
+    
+}
---
+++ Dodgeball_Game/Dodgeball_Elements/Assets/Scripts/Player/Ability_Reaction_Behaviour.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability_Reaction_Behaviour : MonoBehaviour
+{
+
+    [Header("Elemental Reaction Variables")]
+    [SerializeField]
+    [Tooltip("How long the player will be unable to throw or melee")]
+    private float m_Disable_Timer;
+    [SerializeField]
+    [Tooltip("How long the player will be unable to move")]
+    private float m_Stun_Timer;
+    [SerializeField]
+    [Tooltip("How long the player will be pulled in a random direction.")]
+    private float m_Burn_Timer;
+    [SerializeField]
+    [Tooltip("How long the player will have their controls swapped")]
+    private float m_Reversal_Timer;
+
+
+    //current cooldown timers for reactions/ effects
+    private float m_Cur_Disable_Timer, m_Cur_Stun_Timer, m_Cur_Burn_Timer, m_Cur_Reversal_Timer;
+    //booleans for whether or not the player is affected by an element (individual due to many effects possible at once)
+    private bool m_Is_Disabled, m_Is_Stunned, m_Is_Burned, m_Is_Reversed;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Check_Cooldowns();
+    }
+
+
+    void Check_Cooldowns()
+    {
+        //timer for player being disabled (can't throw, melee, or use abilities)
+        if (m_Cur_Disable_Timer < m_Disable_Timer && m_Is_Disabled)
+        {
+            m_Cur_Disable_Timer += Time.deltaTime;
+            //disable player
+        }
+
+        if (m_Cur_Disable_Timer >= m_Disable_Timer && m_Is_Disabled)
+        {
+            Reset_Disable_Variables();
+        }
+
+        //timer for player being Stunned (can't move)
+        if (m_Cur_Stun_Timer < m_Stun_Timer && m_Is_Stunned)
+        {
+            m_Cur_Stun_Timer += Time.deltaTime;
+            //stun player
+        }
+
+        if (m_Cur_Stun_Timer >= m_Stun_Timer && m_Is_Stunned)
+        {
+            Reset_Stun_Variables();
+        }
+
+        //timer for player being burned (will force player to move in random direction *with slight control* )
+        if (m_Cur_Burn_Timer < m_Burn_Timer && m_Is_Burned)
+        {
+            m_Cur_Burn_Timer += Time.deltaTime;
+            //burn player (repeatingly)
+        }
+
+        if (m_Cur_Burn_Timer >= m_Burn_Timer && m_Is_Burned)
+        {
+            Reset_Burn_Variables();
+        }
+
+        //timer for player being reversed (movement inputs or buttons reversed)
+        if (m_Cur_Reversal_Timer < m_Reversal_Timer && m_Is_Reversed)
+        {
+            m_Cur_Reversal_Timer += Time.deltaTime;
+            //reverse player controls
+        }
+
+        if (m_Cur_Reversal_Timer >= m_Reversal_Timer && m_Is_Reversed)
+        {
+            Reset_Reversal_Values();
+        }
+    }
+
+    public void Initiate_Disable(float _Disable_Time)
+    {
+        m_Disable_Timer = _Disable_Time;
+        m_Is_Disabled = true;
+    }
+
+    void Disable_Player()
+    {
+
+    }
+
+    void Reset_Disable_Variables()
+    {
+        m_Cur_Disable_Timer = 0;
+        m_Is_Disabled = false;
+    }
+
+    public void Initiate_Stun(float _Stun_Time)
+    {
+        m_Stun_Timer = _Stun_Time;
+        m_Is_Stunned = true;
+    }
+
+    void Reset_Stun_Variables()
+    {
+        m_Cur_Stun_Timer = 0;
+        m_Is_Stunned = false;
+    }
+
+    public void Initiate_Burn(float _Burn_Time)
+    {
+        m_Burn_Timer = _Burn_Time;
+        m_Is_Burned = true;
+    }
+
+    void Reset_Burn_Variables()
+    {
+        m_Cur_Burn_Timer = 0;
+        m_Is_Burned = false;
+    }
+
+    public void Initiate_Input_Reversal(float _Reversal_Time)
+    {
+        m_Reversal_Timer = _Reversal_Time;
+        m_Is_Reversed = true;
+    }
+
+    void Reset_Reversal_Values()
+    {
+        m_Cur_Reversal_Timer = 0;
+        m_Is_Reversed = false;
+    }
+
+}
